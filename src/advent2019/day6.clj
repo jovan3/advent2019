62f32b61d83ae19b9689bfd4a99898e20ab8b069
Yovan Naumovski <jovan3.14@gmail.com>
2019-12-26 22:08:59 +0200

    cleanup day 6

--- src/advent2019/day6.clj
+++ src/advent2019/day6.clj
@@ -8,4 +8,11 @@
 
 (defn day6 [input]
   (let [g (apply g/digraph (process-input input))]
-    (println "day 6 part 1:" (count (flatten (mapcat vals (vals (alg/all-pairs-shortest-paths g))))))))
+    (println "day 6 part 1:"
+             (->>
+              g
+              alg/all-pairs-shortest-paths
+              vals
+              (mapcat vals)
+              flatten
+              count))))
